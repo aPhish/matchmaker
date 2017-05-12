bc83d2d1b262d6fbde4c7d116d1ea2d3d36879f9
Stephen Irvine <stephen.irvine@autodesk.com>
2017-05-12 13:59:39 +0100

    A list of people

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,45 @@
 
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+
+class Person {
+public:
+    Person(string name) {
+        m_name = name;
+        m_skill = 0;
+    }
+
+    void dump() {
+        cout << m_name << " (" << m_skill << ")" << endl;
+    }
+
+private:
+
+    double m_skill;
+    string m_name;
+};
 
 int main(int argc, char** argv) {
 
 
-   cout << "Hello world" << endl;
+  vector<Person> people = {
+      Person("Steve"),
+      Person("Pernille"), 
+      Person("Anders"), 
+      Person("Katherine"), 
+      Person("Bea"), 
+      Person("Lun"),
+      Person("Ka"),
+      Person("Hieng")
+  };
+ 
+
+  for (Person p : people) {
+      p.dump();
+  }
+   
+   
 
 }
